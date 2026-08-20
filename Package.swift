19e86e97543fa689f8ee92679177718bc90dbb19
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:08:38 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,21 +1,21 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-single-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
             name: "Single Primitives",
             targets: ["Single Primitives"]
-        ),
+        )
     ],
     dependencies: [],
     targets: [
